5ccf068c1ec3bc7b168a9dad81b69207c8767933
Bryant Moscon <bmoscon@gmail.com>
2012-12-14 11:04:26 -0500

    checksum tweaks

--- hash/checksum.cpp
+++ hash/checksum.cpp
@@ -1,3 +1,52 @@
+
+/*
+ * checksum.cpp
+ *
+ *
+ * Simple Checksums
+ *
+ *
+ * Copyright (C) 2012  Bryant Moscon - bmoscon@gmail.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions, and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution, and in the same 
+ *    place and form as other copyright,
+ *    license and disclaimer information.
+ *
+ * 3. The end-user documentation included with the redistribution, if any, must 
+ *    include the following acknowledgment: "This product includes software 
+ *    developed by Bryant Moscon (http://www.bryantmoscon.org/)", in the same 
+ *    place and form as other third-party acknowledgments. Alternately, this 
+ *    acknowledgment may appear in the software itself, in the same form and 
+ *    location as other such third-party acknowledgments.
+ *
+ * 4. Except as contained in this notice, the name of the author, Bryant Moscon,
+ *    shall not be used in advertising or otherwise to promote the sale, use or 
+ *    other dealings in this Software without prior written authorization from 
+ *    the author.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+ * THE SOFTWARE.
+ *
+ */
+
 #include <string>
 #include <stdint.h>
 
@@ -7,7 +56,6 @@ uint8_t checksum_8(const std::string &s) {
   for (uint32_t i = 0; i < s.size(); ++i) {
     checksum = (checksum >> 1) + ((checksum & 1) << 7);
     checksum += s[i];
-    checksum &= 0xff;
   }
 
   return (checksum);
@@ -20,7 +68,6 @@ uint16_t checksum_16(const std::string &s) {
   for (uint32_t i = 0; i < s.size(); ++i) {
     checksum = (checksum >> 1) + ((checksum & 1) << 15);
     checksum += s[i];
-    checksum &= 0xffff;
   }
 
   return (checksum);
@@ -33,7 +80,6 @@ uint32_t checksum_32(const std::string &s) {
   for (uint32_t i = 0; i < s.size(); ++i) {
     checksum = (checksum >> 1) + ((checksum & 1) << 31);
     checksum += s[i];
-    checksum &= 0xffffffff;
   }
 
 uint64_t checksum_64(const std::string &s) {
@@ -42,7 +88,6 @@ uint64_t checksum_64(const std::string &s) {
   for (uint32_t i = 0; i < s.size(); ++i) {
     checksum = (checksum >> 1) + ((checksum & 1) << 63);
     checksum += s[i];
-    checksum &= 0xffffffffffffffff;
   }
 
   return (checksum);
